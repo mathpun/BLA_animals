41b06887bfb48095082404919a69989b42fe4e1d
mathpun <eulersmail@gmail.com>
2016-06-27 13:40:54 -0400

    fixed errror
    ntest

--- classify20way.js
+++ classify20way.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
 	data.imglist_test = getlist_test(condition,ntask,nway);
 	data.imglist_list_train = getlist_train(condition,ntask,nway);
 	data.imglist_demo = getlist_test_demo(latin_id,greek_id,snake_id,bird_id,lizard_id);
-	data.imglist_list_demo = getlist_train_demo(nway);
+	data.imglist_list_demo = getlist_train_demo(ntest);
 	task.load_images(data);
 	console.log("imglist_test:"+data.imglist_test);
 	console.log("imglist_train:"+data.imglist_list_train);
